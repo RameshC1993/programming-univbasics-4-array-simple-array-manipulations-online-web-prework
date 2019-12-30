99b7f0e0a4ff8c546dad906905b9faf350a7a6d3
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2019-12-30 22:48:50 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,35 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+
+def using_pop(array)
+  newarr = array.pop 
+  newarr
+end
+
+def pop_with_args(array)
+  newarr = array.pop(2)
+  newarr
+end
+
+def using_shift(array)
+  newarr = array.shift 
+  newarr
+end
+
+def shift_with_args(array)
+  newarr = array.shift(2)
+  newarr
+end
+
+def using_concat(arr1, arr2)
+  arr1.concat(arr2)
+end
+
+
